2d41015161854d2ca127e8deea03f9f130721101
seferlisk <seferlisk@gmail.com>
2024-11-13 10:37:43 +0200

    added Project and Comment models, scaffolded log-in and log-out pages

---
+++ Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkToDo.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int TaskId { get; set; } // Foreign key for Task
+        public Task Task { get; set; }
+        public int UserId { get; set; } // Foreign key for User
+        public User User { get; set; }
+    }
+
+}
