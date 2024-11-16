d63f903da2937e35fd91112a89586a5709919a07
seferlisk <seferlisk@gmail.com>
2024-11-16 08:59:51 +0200

    added TaskController and moved index view of tasks under folder Task

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WorkToDo.Models;
+using WorkToDo.Data;
+
+namespace WorkToDo.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        public IActionResult Index()
+        {
+            // Retrieve all tasks assigned to the logged-in user
+            var userId = User.Identity?.Name; // Assuming "Name" is the username or email
+            var tasks = _context.Tasks
+                .Where(t => t.AssignedTo == userId)
+                .OrderBy(t => t.DueDate)
+                .ToList();
+
+            return View(tasks);
+        }
+
+        // Additional actions for Create, Edit, Delete, etc., can be added here
+    }
+}
