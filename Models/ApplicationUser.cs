53b3140a488359f080178a3ce5d1260ce7693c9d
seferlisk <seferlisk@gmail.com>
2024-12-03 13:47:36 +0200

    minor changes to ApplicationUser

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,9 +3,7 @@ namespace WorkToDo.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+       
         public ICollection<Assignment> AssignedTasks { get; set; } // Tasks assigned to the user
     }
 
